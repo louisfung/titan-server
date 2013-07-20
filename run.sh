c125b4ef525372ba756591b03f1a955885fd29b9
Peter <mcheung63@hotmail.com>
2013-07-21 02:48:01 +0800

    Signed-off-by: Peter <mcheung63@hotmail.com>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "generate pandora-server.xml"
-echo "	java -jar target/pandora-server.jar -s"
+echo "generate titan-server.xml"
+echo "	java -jar target/titan-server.jar -s"
 echo
 echo "run it:"
-echo "	java -Djava.library.path=libInUse/hyperic-sigar-1.6.4/sigar-bin/lib/ -jar target/pandora-server.jar"
+echo "	java -Djava.library.path=libInUse/hyperic-sigar-1.6.4/sigar-bin/lib/ -jar target/titan-server.jar"
