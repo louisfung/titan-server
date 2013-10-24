c598cb36dcf1207cb161b0c1cffe85758248a736
root <root@ubuntu.(none)>
2013-10-24 14:25:20 +0800

    changed version number

--- src/main/java/com/titanserver/Global.java
+++ src/main/java/com/titanserver/Global.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 import java.util.Hashtable;
 
 public class Global {
-	public static String version = "20130717";
+	public static String version = "20131012";
 	public static String stage = "dev";
 	public static Hashtable<Integer, Socket> clients = new Hashtable<Integer, Socket>();
 }
