8d805167e1df16708bcaf907ab70e8f876691567
Peter <root@peter9.(none)>
2013-11-04 22:41:47 +0800

    start.sh

--- test/start.sh
+++ test/start.sh
@@ -1,1 +1,1 @@
-java -Djava.library.path=/root/workspace/titan-server/libInUse/hyperic-sigar-1.6.4/sigar-bin/lib/ -jar ../target/titan-server20131012.jar 
+java -Djava.library.path=/root/git/titan-server/libInUse/hyperic-sigar-1.6.4/sigar-bin/lib/ -jar ../target/titan-server20131012.jar 
