d96e4e2721b9261a6271775c19e763ff32406b03
Peter <mcheung63@hotmail.com>
2013-11-23 23:57:19 +0800

    Signed-off-by: Peter <mcheung63@hotmail.com>

--- src/main/java/com/titanserver/Global.java
+++ src/main/java/com/titanserver/Global.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 import java.util.Hashtable;
 
 public class Global {
-	public static String version = "20131012";
+	public static String version = "20131123";
 	public static String stage = "dev";
 	public static Hashtable<Integer, Socket> clients = new Hashtable<Integer, Socket>();
 }
