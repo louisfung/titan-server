79c592371b2fa0d0efd0f64733d0ee44a798dd13
Peter <mcheung63@hotmail.com>
2013-12-14 23:22:58 +0800

    Signed-off-by: Peter <mcheung63@hotmail.com>

--- src/main/java/com/titanserver/HttpResult.java
+++ src/main/java/com/titanserver/HttpResult.java
@@ -1,8 +1,10 @@
 package com.titanserver;
 
+import java.io.Serializable;
+
 import org.apache.http.Header;
 
-public class HttpResult {
+public class HttpResult implements Serializable {
 	public String content;
 	public Header headers[];
 }
